f17b1e5d071ecd1742e7901e50951bc722ee1e49
anagtemboury <anagtemboury@uma.es>
2025-03-12 22:48:04 +0100

    He añadido un metodo a Date.java

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -20,6 +20,10 @@ public class Date {
   private final int month;
   private final int year;
 
+  public void methodAddedByOwner(){
+    System.out.println("Method added by the owner");
+  }
+
   /**
    * Retrieves the day component of this date.
    *
@@ -83,9 +87,5 @@ public class Date {
     } catch (ParseException e) {
       return false;
     }
-  }
-
-  public void methodAddedByOwner(){
-    System.out.println("Method added by the owner");
   }
 }
